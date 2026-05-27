f5752d7b4ceef5e8edc54889161e24057954706d
Fred Otieno <obachi62@gmail.com>
2026-05-27 15:26:56 +0300

    Add /api/seed route to populate database

--- shopease-backend/server.js
+++ shopease-backend/server.js
@@ -6,6 +6,7 @@ import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import seedRoutes from './routes/seedRoutes.js';
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/seed', seedRoutes);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
