1f5e1d549a85d88e0e2a88dbb7bce887eb251f06
Fred Otieno <obachi62@gmail.com>
2026-06-18 12:29:06 +0300

    chore: revert base URL back to shopease-api-otyi.onrender.com

--- shopease-frontend/src/utils/api.js
+++ shopease-frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://shopease-api-skey.onrender.com/api',
+  baseURL: 'https://shopease-api-otyi.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
